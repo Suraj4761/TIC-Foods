210ada6b7abf11144708003ea8940c5cfba238fa
Akshu <akshaychavan.work@gmail.com>
2024-10-05 14:37:32 +0530

    banner for landing page

--- tic-foods/src/app/page.tsx
+++ tic-foods/src/app/page.tsx
@@ -1,9 +1,11 @@
-import HomePage from "./screens/homepage"
+import HomePage from "./screens/homepage" 
+import BannerPost from "./screens/banner"
 
 export default function Home() {
   return (
     <>
     <HomePage />
+    <BannerPost/>
     </>
   );
 }
